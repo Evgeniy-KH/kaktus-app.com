b1f11002e0e862a62290db8196312701c4dae0dc
2806t <000087@ukr.net>
2023-01-26 16:01:41 +0100

    code style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,23 +12,23 @@
                         <div class="form-group">
                             <label for="email"
                                    class="col-form-label tm-text-primary">{{ __('Email Address') }}</label>
-
                             <div>
                                 <input id="email" type="email"
                                        class=" form-control rounded-0 @error('email') is-invalid @enderror" name="email"
                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="password"
                                    class="col-form-label text-md-end tm-text-primary">{{ __('Password') }}</label>
-
                             <div>
                                 <input id="password" type="password"
                                        class="form-control  rounded-0 @error('password') is-invalid @enderror"
@@ -40,6 +40,7 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+
                             </div>
                         </div>
 
@@ -48,8 +49,8 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember"
                                            id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label tm-text-primary" for="remember" style="font-weight: 800;">
+                                    <label class="form-check-label tm-text-primary" for="remember"
+                                           style="font-weight: 800;">
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
@@ -58,7 +59,8 @@
 
                         <div class="form-group mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary" style="background-color: #0397ed; border: none">
+                                <button type="submit" class="btn btn-primary"
+                                        style="background-color: #0397ed; border: none">
                                     {{ __('Login') }}
                                 </button>
 
@@ -67,6 +69,7 @@
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+
                             </div>
                             <div class="flex items-center justify-end mt-4">
                                 <a class="btn" href="{{ url('auth/facebook') }}"
@@ -78,7 +81,6 @@
                     </form>
                 </div>
             </div>
-
         </div>
     </div>
 @endsection
