0c252fa6de354e95f3a67e1bd40bebd8b0491cad
2806t <000087@ukr.net>
2023-02-13 21:04:03 +0100

    style

--- app/Filters/Dish/TagFilter.php
+++ app/Filters/Dish/TagFilter.php
@@ -6,13 +6,12 @@ class TagFilter
 {
     function __invoke($query, $request)
     {
-        $tag_id= $request;
+        $tag_id = $request;
 
         $filter = function ($q) use ($tag_id) {
             $q->where('tag_id', $tag_id);
         };
 
        $query->whereHas('tags', $filter)->with('getDishImages', 'tags');
-
     }
 }
