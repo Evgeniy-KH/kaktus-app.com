ee716608ef43da83b37e31910e5203df87825cbd
2806t <000087@ukr.net>
2023-02-13 20:52:04 +0100

    KeyworfFilter

---
+++ app/Filters/Dish/KeywordFilter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filters\Dish;
+
+class KeywordsFilter
+{
+    function __invoke($query, $request)
+    {
+        $tag_id= $request;
+
+        $filter = function ($q) use ($tag_id) {
+            $q->where('tag_id', $tag_id);
+        };
+
+       $query->whereHas('tags', $filter)->with('getDishImages', 'tags');
+
+    }
+}
