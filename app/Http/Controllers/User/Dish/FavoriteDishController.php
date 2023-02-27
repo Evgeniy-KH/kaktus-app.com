f0fb568149d5f8d9630e55981718fa5fe6f6df54
2806t <000087@ukr.net>
2023-02-27 21:37:32 +0100

    ADD favorite model, controller, view and jquery

---
+++ app/Http/Controllers/User/Dish/FavoriteDishController.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\User\Dish;
+
+
+use App\Filters\DishFilter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Dish\StoreRequest;
+use App\Http\Requests\Dish\UpdateRequest;
+use App\Models\Dish;
+use App\Models\Tag;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+
+class FavoriteDishController extends BaseController
+{
+
+    public function create(): View
+    {
+        $user = Auth::user();
+
+        return view('dish.create', compact('user'));
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+        $this->service->store($data);
+
+        return response()->json();
+    }
+
+    public function editView(int $dishId)
+    {
+        return view('dish.edit', compact('dishId'));
+    }
+
+    public function editData(int $dishId)
+    {
+        $dish = Dish::with('getDishImages')->findOrFail($dishId);
+        $tags = Tag::all();
+        $returnData = [$dish, $tags];
+
+        return response()->json($returnData);
+    }
+
+    public function update(UpdateRequest $request)
+    {
+        $data = $request->validated();
+        $dish = Dish::findOrFail($data['dish_id']);
+        unset($data['dish_id']);
+
+        $dish = $this->service->update($data, $dish);
+
+        return response()->json();
+    }
+
+    public function delete(int $dishId)
+    {
+        $dish = Dish::findOrFail($dishId)->delete();
+
+        return response()->json();
+    }
+
+}
