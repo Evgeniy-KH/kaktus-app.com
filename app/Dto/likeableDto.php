45e378e7611b7a65a491a8847d4ca1f2e531979c
2806t <000087@ukr.net>
2023-04-03 11:39:43 +0200

    likeable_type_id : change the name anf type of column likeable_type

---
+++ app/Dto/likeableDto.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Dto;
+
+use Spatie\LaravelData\Data;
+use Illuminate\Http\UploadedFile;
+
+class likeableDto extends Data
+{
+
+    public function __construct(
+        private readonly int          $dishId,
+        private readonly int          $typeId,
+        private readonly string       $path,
+    )
+    {
+    }
+
+    public final function getDishId(): int
+    {
+        return $this->dishId;
+    }
+
+    public final function getTypeId(): int
+    {
+        return $this->typeId;
+    }
+
+    public final function getPath(): string
+    {
+        return $this->path;
+    }
+}
